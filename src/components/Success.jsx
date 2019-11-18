96db08757a8c1bf051e5db77728b3aaa6c2986e4
shubhranshu <shuboypanda@gmail.com>
2019-11-18 15:34:46 +0530

    initial commit

---
+++ src/components/Success.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Success extends Component {
+    render(){
+        return(
+            <div>
+                <h1 className="ui centered"> Details Successfully saved..</h1>
+            </div>
+        )
+    }
+}
+
+export default Success;
